31d86fb63b413f450f3df6b1692e81c1919e15a2
FeFeFarias05 <fernanda.farias05@hotmail.com>
2024-06-19 21:03:59 -0300

    alterações

--- main.java
+++ main.java
@@ -1,11 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class main{
-    public static void main(String args[]){
-        Caixa teste = new Caixa();
-        String[] valores = teste.getMedidas("225 889 888");
+
+
+    static String[] getMedidas(String medidas){
+      String[] medidasSeparadas = medidas.split(" ");
+
+      Arrays.sort(medidasSeparadas);
+
+        return medidasSeparadas;
+    }
+
+    public static void main(String args[]) throws FileNotFoundException{
+        //Caixa teste = new Caixa();
+
+        Digraph grafo = new Digraph();
+
+        String[] valores = getMedidas("225 889 888");
 
         for(int i =0; i<valores.length; i++)
         {
             System.out.println("Valores -> " + valores[i]);
+        }
+
+        File arq =  new File("caso00010.txt");
+        Scanner t = new Scanner(arq);
+
+        while(t.hasNextLine())
+        {
+            String linha = t.nextLine();
+            System.out.println(linha);
         }
     }
 }
