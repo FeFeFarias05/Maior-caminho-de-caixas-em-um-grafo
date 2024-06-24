e97e085654495953017b56417235914520c09366
FeFeFarias05 <fernanda.farias05@hotmail.com>
2024-06-24 19:32:43 -0300

    contador de tempo

--- App.java
+++ App.java
@@ -3,8 +3,13 @@ import java.io.FileNotFoundException;
 public class App {
 
     public static void main(String[] args) throws FileNotFoundException {
+        long start = System.nanoTime();
         Box digrafo = new Box("casoPDF.txt");
         System.out.println("Maior caminho encontrado: " + digrafo.biggerPath());
         System.out.println(digrafo.toDot());
+
+        long end = System.nanoTime();
+        long dur = end - start;
+        System.out.println("Tempo de duração do teste: "+dur);
     }
 }
